5ffdae86ad77e24fe967afafdf281d2a08aa1f05
Rodrigo Y. Murayama <hekatonkheirex@gmail.com>
2024-03-01 19:35:49 -0300

    Removed swayidle and swaylock, replaced with hypridle and hyprlock

--- .local/bin/swayidle.sh
+++ .local/bin/swayidle.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swayidle -w timeout 300 'swaylock -f' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
+swayidle -w timeout 300 'hyprlock -f' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
